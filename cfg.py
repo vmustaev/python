9c62345f83f66ac62be0a96fbca1fb28aeb83078
Венер Мустаев <mustaev.03@mail.ru>
2021-12-25 17:37:37 +0500

    Signed-off-by: Венер Мустаев <mustaev.03@mail.ru>

---
+++ cfg.py
@@ -0,0 +1,21 @@
+
+import os
+
+
+
+WIDTH = 500
+HEIGHT = 500
+
+CURRPATH = os.getcwd()
+RESOURCESDIRPATH = os.path.join(CURRPATH, 'resources')
+AUDIOSDIRPATH = os.path.join(RESOURCESDIRPATH, 'audios')
+FONTDIRPATH = os.path.join(RESOURCESDIRPATH, 'font')
+IMAGESDIRPATH = os.path.join(RESOURCESDIRPATH, 'images')
+BALLPICPATH = os.path.join(IMAGESDIRPATH, 'ball.png')
+RACKETPICPATH = os.path.join(IMAGESDIRPATH, 'racket.png')
+FONTPATH = os.path.join(FONTDIRPATH, 'font.TTF')
+GOALSOUNDPATH = os.path.join(AUDIOSDIRPATH, 'goal.wav')
+HITSOUNDPATH = os.path.join(AUDIOSDIRPATH, 'hit.wav')
+BGMPATH = os.path.join(AUDIOSDIRPATH, 'bgm.mp3')
+
+WHITE = (255, 255, 255)
